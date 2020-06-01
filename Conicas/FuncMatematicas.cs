a3ecbf593c83347eb48086b2186838efed6f3a73
Giovanni Shibaki <giovannishibaki@gmail.com>
2020-06-01 19:39:50 -0300

    Calcula a' e c'
    Função para que a' e c' sejam calculados seguindo as notas de aula de GA na página 94

--- Conicas/FuncMatematicas.cs
+++ Conicas/FuncMatematicas.cs
@@ -18,6 +18,10 @@ namespace Conicas
         private double h;
         private double k;
 
+        private double aL; // a'
+        private double cL; // c'
+        // Conforme notas de aula pag 94
+
         // Constructor that takes one argument:
         public FuncMatematicas()
         {
@@ -58,10 +62,27 @@ namespace Conicas
             return det;
         }
 
-        /*public Vector<double> gerarMatrizG2()
+        public Vector<double> gerarG2(double a, double b, double c, double d, double e, double f)
         {
+            var B = Vector<double>.Build.Dense(new double[] { a, b, c, (d*this.h)/2, (e*this.k)/2, f });
+            // Função G2 do tipo a.u^2 + b.u.v + c.v^2 + dh/2 + ek/2 + f ---> Conforme Notas de Aula pag 90
+            return B;
+        }
 
-        }*/
+        public void calculaAlCl(Vector<double> g2)
+        {
+            var A = Matrix<double>.Build.DenseOfArray(new double[,]
+            {
+                { 1/* a' */, 1/* c' */ },
+                { 1/* a' */, -1/* -c' */ },
+            });
+            MessageBox.Show(A.ToString());
+            var B = Vector<double>.Build.Dense(new double[] { (g2[0]+g2[1]), /* g2[0] = termo a de g2;  g2[2] = termo c de g2*/(g2[1]*Math.Sqrt(1 + Math.Pow( ( (g2[0]-g2[2])/g2[1]),2) ) ) });
+            var x = A.Solve(B);
+            MessageBox.Show("a'  e  c' : "+x.ToString());
+            aL = x[0];
+            cL = x[1];
+        }
 
     }
 }
