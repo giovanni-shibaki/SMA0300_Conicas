101f8184f862a6d63ca3d64fa6c635cf181ff24e
Muramatsu2602 <pedromuramatsuc@gmail.com>
2020-06-03 19:37:53 -0300

    Graphing

--- Conicas/ElementosGeometricos.cs
+++ Conicas/ElementosGeometricos.cs
@@ -35,62 +35,6 @@ namespace Conicas
             funcMat = new FuncMatematicas();
         }
 
-        public int whatConica(double[] coeficientes)
-        {
-            double det = funcMat.acharSolucoesSistema(coeficientes[0], coeficientes[1], coeficientes[2]);
-            funcMat.calculaH_K(coeficientes[0],coeficientes[1],coeficientes[2],coeficientes[3],coeficientes[4],coeficientes[5]);
-
-            this.h = funcMat.getH();
-            this.k = funcMat.getK();
-
-            // Conicas com centro unico
-            if (det != 0)
-            {
-                // se nao tem termo independente
-                // se f = 1: hiperbole ou elipse
-                if (coeficientes[5] == 1)
-                {
-                    // se x^2 e x^2 sao positivos, temos elipse
-                    if(coeficientes[0]>0 && coeficientes[1] > 0) { return 6; }
-                    // se x^2 e y^2 tem sinais contrarios, temos hiperbole
-                    if(coeficientes[0]>0 && coeficientes[1]<0 || coeficientes[0] > 0 && coeficientes[1] < 0) { return 7; }
-                }
-                // se f<0 e x^2>0 e y^2>0, pode ser uma circunferencia
-                if (coeficientes[5] < 0 && coeficientes[0]>0 && coeficientes[1] > 0)
-                {
-                    return 5;
-                }
-                // se x^2 e y^2 tem coeficientes opostos,entao temos retas concorrentes
-                if(coeficientes[0] > 0 && coeficientes[1] < 0 || coeficientes[0] > 0 && coeficientes[1] < 0) { return 4; }
-                
-                // se x^2 e y^2 sao positivos e nao temos f, temos um ponto  
-                if (coeficientes[5] == 0 && coeficientes[0] > 0 && coeficientes[1] > 0) { }
-
-                // Conicas com centros infinitos ou conicas sem centro
-            }
-            else if (det == 0)
-            {   
-                // se o sistema de h e k da SPI
-                if(Double.IsInfinity(h) && Double.IsInfinity(k))
-                {
-                    // se temos um f, ele 'e o coeficiente linear da reta: retas paralelas
-                    if (coeficientes[5] != 0)
-                    {
-                        return 3;
-                    }
-                    // senao, sao retas identicas
-                    else
-                    {
-                        return 2;
-                    }
-                }else if(Double.IsNaN(h) && Double.IsNaN(k))
-                {
-                    return 8;
-                }
-            }
-            return -1;
-        }
-
         public string DetalhesConicas(double[] coeficientes)
         {
             idConica = whatConica(coeficientes);
@@ -129,24 +73,85 @@ namespace Conicas
             return "Erro";
         }
 
+        public int whatConica(double[] coeficientes)
+        {
+            double det = funcMat.acharSolucoesSistema(coeficientes[0], coeficientes[1], coeficientes[2]);
+            funcMat.calculaH_K(coeficientes[0], coeficientes[1], coeficientes[2], coeficientes[3], coeficientes[4], coeficientes[5]);
+
+            this.h = funcMat.getH();
+            this.k = funcMat.getK();
+
+            // Conicas com centro unico
+            if (det != 0)
+            {
+                // se nao tem termo independente
+                // se f = 1: hiperbole ou elipse
+                if (coeficientes[5] == 1)
+                {
+                    // se x^2 e x^2 sao positivos, temos elipse
+                    if (coeficientes[0] > 0 && coeficientes[1] > 0) { return 6; }
+                    // se x^2 e y^2 tem sinais contrarios, temos hiperbole
+                    if (coeficientes[0] > 0 && coeficientes[1] < 0 || coeficientes[0] > 0 && coeficientes[1] < 0) { return 7; }
+                }
+                // se f<0 e x^2>0 e y^2>0, pode ser uma circunferencia
+                if (coeficientes[5] < 0 && coeficientes[0] > 0 && coeficientes[1] > 0)
+                {
+                    return 5;
+                }
+                // se x^2 e y^2 tem coeficientes opostos,entao temos retas concorrentes
+                if (coeficientes[0] > 0 && coeficientes[1] < 0 || coeficientes[0] > 0 && coeficientes[1] < 0) { return 4; }
+
+                // se x^2 e y^2 sao positivos e nao temos f, temos um ponto  
+                if (coeficientes[5] == 0 && coeficientes[0] > 0 && coeficientes[1] > 0) { }
+
+                // Conicas com centros infinitos ou conicas sem centro
+            }
+            else if (det == 0)
+            {
+                // se o sistema de h e k da SPI
+                if (Double.IsInfinity(h) && Double.IsInfinity(k))
+                {
+                    // se temos um f, ele 'e o coeficiente linear da reta: retas paralelas
+                    if (coeficientes[5] != 0)
+                    {
+                        return 3;
+                    }
+                    // senao, sao retas identicas
+                    else
+                    {
+                        return 2;
+                    }
+                }
+                else if (Double.IsNaN(h) && Double.IsNaN(k))
+                {
+                    return 8;
+                }
+            }
+            return -1;// ERRO
+        }
+
         #region detalhes de cada conica
         private string DetalhesParabol(double[] coeficientes)
         {
+            //
             throw new NotImplementedException();
         }
 
         private string DetalhesHiperbole(double[] coeficientes)
         {
+            // diretriz, focos, assintotas
             throw new NotImplementedException();
         }
 
         private string DetalhesElipse(double[] coeficientes)
         {
+            // focos, vertices, excentricidade
             throw new NotImplementedException();
         }
 
         private string DetalhesCirc(double[] coeficientes)
         {
+            //raio, centro.
             throw new NotImplementedException();
         }
 
@@ -169,7 +174,8 @@ namespace Conicas
 
         private string DetalhesRetIdentica(double[] coeficientes)
         {
-            throw new NotImplementedException();
+            string detalhes = "Temos duas retas identicas, isto é, paralelas e coicidentes";
+            return detalhes;
         }
 
         string DetalhesConjVazio(double[] coeficientes)
