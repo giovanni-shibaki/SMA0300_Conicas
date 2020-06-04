f4f97ab8b1d2230950d9a8f04f30b18e00aee583
Muramatsu2602 <pedromuramatsuc@gmail.com>
2020-06-04 16:02:21 -0300

    detalhes almost ready

--- Conicas/ElementosGeometricos.cs
+++ Conicas/ElementosGeometricos.cs
@@ -75,29 +75,11 @@ namespace Conicas
 
         public int whatConica(double[] coeficientes)
         {
-        
             double big_det = funcMat.whole_matrix_determinant(coeficientes);
             double det = funcMat.acharSolucoesSistema(coeficientes[0], coeficientes[1], coeficientes[2]);
-            funcMat.calculaH_K(coeficientes);
-
-            this.h = funcMat.getH();
-            this.k = funcMat.getK();
-
-            // Conicas com centro unico
-<<<<<<< HEAD
-            
-            if (big_det != 0)
-            {
-                // conj vazio
-                if(coeficientes[0]==coeficientes[1] && coeficientes[5] == coeficientes[0]) { return 0; }
-                // ponto
-                if (det==0 && coeficientes[0] == coeficientes[1] && coeficientes[5] == 0) { return 1; }
-
-=======
 
             if (big_det != 0)
             {
->>>>>>> master
                 // circ
                 if (coeficientes[0] == coeficientes[2] && coeficientes[1] == 0) { return 5; }
                 // elipse
@@ -106,35 +88,32 @@ namespace Conicas
                 else if (det < 0) { return 7; }
                 // parabola
                 else if (det == 0) { return 8; }
-                //Retas Concorrentes: se x^2 e y^2 tem coeficientes opostos,entao temos 
-                //else if (coeficientes[0] > 0 && coeficientes[1] < 0 || coeficientes[0] > 0 && coeficientes[1] < 0) { return 4; }
-                //Ponto: se x^2 e y^2 sao positivos e nao temos f
-                if (coeficientes[5] == 0 && coeficientes[0] > 0 && coeficientes[1] > 0) { return 1; }
             }
+            //  Conicas Degeneradas
             else if (big_det == 0)
             {
-                //  Conicas com centros infinitos ou conicas sem centro
-
-                // se o sistema de h e k da SPI
-                if (Double.IsInfinity(h) && Double.IsInfinity(k))
+                // conj vazio
+                if (coeficientes[0] == coeficientes[1] && coeficientes[5] == coeficientes[0]) { return 0; }
+                // ponto 
+                if (det > 0) { return 1; }
+                // retas paralelas
+                if (det == 0)
                 {
-                    // se temos um f, ele 'e o coeficiente linear da reta: retas paralelas
-                    if (coeficientes[5] != 0)
+                    // D^2 + E^2 > 4(A+C)F
+                    // distintas se
+                    if ((Math.Pow(coeficientes[3], 2) + Math.Pow(coeficientes[3], 2)) > 4 * (coeficientes[0] + coeficientes[2]) * coeficientes[5])
                     {
                         return 3;
                     }
-                    // senao, sao retas identicas
-                    else
+                    // coincidentes se
+                    else if ((Math.Pow(coeficientes[3], 2) + Math.Pow(coeficientes[3], 2)) == 4 * (coeficientes[0] + coeficientes[2]) * coeficientes[5])
                     {
                         return 2;
                     }
                 }
-                else if (Double.IsNaN(h) && Double.IsNaN(k))
-                {
-                    return 8;
-                }
+                // retas concorrentes
+                if (det < 0) { return 4; }
             }
-                      
             return -1;// ERRO
         }
 
@@ -175,8 +154,8 @@ namespace Conicas
 
         private string DetalhesPonto(double[] coeficientes)
         {
-            int X=0,Y=0;
-            string detalhes = "P("+X+","+Y+")";
+            int X = 0, Y = 0;
+            string detalhes = "P(" + X + "," + Y + ")";
             return detalhes;
         }
 
@@ -188,10 +167,8 @@ namespace Conicas
 
         string DetalhesConjVazio(double[] coeficientes)
         {
-            string detalhes="Conjunto Vazio";
+            string detalhes = "Conjunto Vazio";
             return detalhes;
         }
         #endregion
 
-    }
-}
